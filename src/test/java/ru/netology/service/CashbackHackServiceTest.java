6794019844ebe704459a2309f1a9990fd82f53da
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-07-04 18:27:31 +0300

    create branch testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
